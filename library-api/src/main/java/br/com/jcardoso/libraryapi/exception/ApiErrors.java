67118910bae2b601fd3e234e58a4f30667208fef
jaderson.brandao <jaderson.brandao@gmail.com>
2021-11-29 15:01:08 -0300

    Utilizando recursos Bean Validation e Exception Handler

---
+++ library-api/src/main/java/br/com/jcardoso/libraryapi/exception/ApiErrors.java
@@ -0,0 +1,21 @@
+package br.com.jcardoso.libraryapi.exception;
+
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiErrors {
+
+    private  List<String> errors;
+
+    public ApiErrors(BindingResult bindingResult) {
+        this.errors = new ArrayList<>();
+        bindingResult.getAllErrors().forEach( error -> this.errors.add(error.getDefaultMessage()));
+
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
--- library-api/src/main/java/br/com/jcardoso/libraryapi/resource/BookController.java
+++ library-api/src/main/java/br/com/jcardoso/libraryapi/resource/BookController.java
@@ -3,10 +3,17 @@ package br.com.jcardoso.libraryapi.resource;
 
 import br.com.jcardoso.libraryapi.dto.BookDTO;
 import br.com.jcardoso.libraryapi.entity.Book;
+import br.com.jcardoso.libraryapi.exception.ApiErrors;
 import br.com.jcardoso.libraryapi.service.BookService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -25,10 +32,17 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(@RequestBody BookDTO dto) {
+    public BookDTO create(@RequestBody @Valid BookDTO dto) {
         Book entity = modelMapper.map(dto, Book.class);
         entity = service.save(entity);
         return modelMapper.map(entity, BookDTO.class);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleValidationException(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult =  ex.getBindingResult();
+        return new ApiErrors(bindingResult);
     }
 
 }
